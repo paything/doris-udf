3d4f443adaac943db454c7db2772b3a9ff595104
PayneWong <419552620@qq.com>
2025-06-24 21:18:24 +0800

    完善test.sql

--- test.sql
+++ test.sql
@@ -12,46 +12,6 @@ CREATE FUNCTION window_funnel(INT, INT, STRING, STRING) RETURNS STRING PROPERTIE
   "type"="JAVA_UDF"
 );
 
--- 测试语句 - 基本功能测试
-with event_log as (
-select '2024-01-01 00:00:00.123' as dt,'payne' as uid,'reg' as event,'tiktok' as group0
-union all
-select '2024-01-01 00:00:01.345' as dt,'payne' as uid,'iap' as event,'tiktok' as group0
-union all
-select '2024-01-01 00:00:03.111' as dt,'payne' as uid,'chat' as event,'tiktok' as group0
-union all
-select '2024-02-01 00:00:00.012' as dt,'payne' as uid,'reg' as event,'fb' as group0
-union all
-select '2024-02-01 00:00:01.001' as dt,'payne' as uid,'iap' as event,'fb' as group0
-union all
-select '2024-02-01 00:00:02.434' as dt,'payne' as uid,'chat' as event,'fb' as group0
-union all
-select '2024-01-01 00:00:00.012' as dt,'cjt' as uid,'reg' as event,'fb' as group0
-union all
-select '2024-01-01 00:00:01.001' as dt,'cjt' as uid,'iap' as event,'fb' as group0
-union all
-select '2024-01-01 00:00:02.434' as dt,'cjt' as uid,'chat' as event,'fb' as group0
-)
-SELECT 
-    uid,
-    mysql.window_funnel(10, 1, 'strict', funnel_track) as funnel_result
-FROM (
-    SELECT 
-        uid,
-        group_concat(event_string) as funnel_track
-    FROM (
-    select 
-        uid,
-        concat(dt,'#'
-        ,event='reg'
-        ,'@',event='iap'
-        ,'@',event='chat'
-        ,'#',group0
-        ) as event_string
-    from event_log
-) t1
-    GROUP BY uid
-) t;
 
 -- 测试不同分析模式
 -- 1. default模式测试（允许时间相等）
@@ -90,7 +50,7 @@ select '2024-01-01 00:00:00.000' as dt,'user2' as uid,'reg' as event,'test' as g
 union all
 select '2024-01-01 00:00:00.200' as dt,'user2' as uid,'chat' as event,'test' as group0
 union all
-select '2024-01-01 00:00:00.100' as dt,'user2' as uid,'iap' as event,'test' as group0
+select '2024-01-01 00:00:00.300' as dt,'user2' as uid,'iap' as event,'test' as group0
 )
 SELECT 
     'backtrack_mode' as test_type,
